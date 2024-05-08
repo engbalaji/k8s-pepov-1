21b4ecc21a3d3764eb0cb6c2fd4d25a76f597e38
balaji mariyappan <engbalaji@users.noreply.github.com>
2024-05-08 13:22:01 -0400

    Dockerfile  update

---
+++ docker-image/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "2.23.1"
+    }
+  }
+}
+
+provider "docker" {
+  host = "tcp://localhost:2375"
+}
+
+resource "docker_image" "example" {
+  name         = "example:latest"
+  keep_locally = false
+
+  build {
+    context    = path.module
+    dockerfile = ./Dockerfile
+  }
+}
