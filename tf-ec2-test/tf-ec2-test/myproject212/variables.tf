e8f865c7a8f3b50be799182953115e9a44fc2d94
balaji mariyappan <engbalaji@users.noreply.github.com>
2024-05-11 13:45:05 -0400

    Variable update

--- tf-ec2-test/tf-ec2-test/myproject212/variables.tf
+++ tf-ec2-test/tf-ec2-test/myproject212/variables.tf
@@ -1,24 +1,7 @@
 variable "aws_region" {
   default = "us-east-1"
 }
-variable "ami_id" {
-  default = "ami-089d9f6c91edad62b"
-}
-variable "instance_type" {
-  default = "t2.micro"
-}
-variable "subnet1_id" {
-  default = "subnet-89eb03a4"
-}
-variable "security_group1_id" {
-  default = "sg-02cd5c886d8b07682"
-}
-variable "instance1_name" {
-  default = "sspoc1"
-}
-variable "my-key-pair" {
-  default = "Balaji Mariyappan"
-}
+
 variable "instance_id" {
   default = "i-0f0900d30a2fe7915"
 }
