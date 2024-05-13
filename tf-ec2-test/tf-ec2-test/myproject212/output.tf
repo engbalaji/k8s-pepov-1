b7630d0c1a93247470cb306822ca812da915c152
balaji mariyappan <engbalaji@users.noreply.github.com>
2024-05-13 13:08:01 -0400

    Updated the output file

--- tf-ec2-test/tf-ec2-test/myproject212/output.tf
+++ tf-ec2-test/tf-ec2-test/myproject212/output.tf
@@ -1,8 +1,8 @@
 
-output "instance_state" {
-  value = aws_instance.example.state
-}
+#output "instance_state" {
+#  value = aws_instance.example.state
+#}
 
-output "instance_id" {
-  value = aws_instance.example1.id
-}
+#output "instance_id" {
+#  value = aws_instance.example1.id
+#}
