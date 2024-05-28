324489170f7e3071dfb78858ad7469080d040358
balaji mariyappan <engbalaji@users.noreply.github.com>
2024-05-28 15:52:44 -0400

    Multiple changes for the eks creation files

--- learn-terraform-provision-eks-cluster/chartsargo.tf
+++
@@ -1,7 +0,0 @@
-#resource "helm_release" "argocd" {
-#  name       = "argocd-helm"
-#  namespace  = "argocd"
-# chart      = "argo-cd"
-#  create_namespace = true
-#  repository = "https://argoproj.github.io/argo-helm"
-#  }
