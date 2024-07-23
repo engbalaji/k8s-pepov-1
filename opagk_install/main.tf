1cc3f46ff64fed5d6c51524ce7b3c2896f8829f9
balaji mariyappan <engbalaji@users.noreply.github.com>
2024-07-23 18:01:24 -0400

    modified:   opagk_install/main.tf

--- opagk_install/main.tf
+++ opagk_install/main.tf
@@ -6,13 +6,12 @@ provider "kubernetes" {
 
 resource "kubernetes_namespace" "gatekeeper" {
   metadata {
-	name = "gatekeeper-system"
+    name = "gatekeeper-system"
   }
 }
 
 module "opa_gatekeeper" {
-  source    = "project-octal/opa-gatekeeper/kubernetes"
+  source    = "github.com/project-octal/terraform-kubernetes-opa-gatekeeper"
   namespace = kubernetes_namespace.gatekeeper.metadata[0].name
   version   = "0.1.0"
-  cluster_ca_certificate = base64decode(var.k8s_ca_certificate)
 }
