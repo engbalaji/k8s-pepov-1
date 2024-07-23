6b08bc930c4a3c6e38fb5e0c1178128aa53f424c
balaji mariyappan <engbalaji@users.noreply.github.com>
2024-07-23 17:43:41 -0400

    modified:   opagk_install/variable.tf

--- opagk_install/variable.tf
+++ opagk_install/variable.tf
@@ -2,5 +2,5 @@
 variable "k8s_ca_certificate" {
   description = "The base64-encoded Kubernetes cluster CA certificate"
   type        = string
-  default = test
+  default = "akJFHASDKJFHKASJDHKAJ"
 }
