9ef7dabcc5feb1208b9d24aa72c336d214036e9e
balaji mariyappan <engbalaji@users.noreply.github.com>
2025-03-03 20:38:23 -0500

    Update main.tf

--- kafkacluster/main.tf
+++ kafkacluster/main.tf
@@ -1,10 +1,10 @@
 provider "kubernetes" {
-  config_path = "~/.kube/config"
+  config_path = "/home/ec2-user/.kube/config"
 }
 
 provider "helm" {
   kubernetes {
-    config_path = "~/.kube/config"
+    config_path = "/home/ec2-user/.kube/config"
   }
 }
 
